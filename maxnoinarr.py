d675da388533515e5430f1abf957042b1e93e487
SanjayG27 <sanjaysekarviji27@gmail.com>
2024-08-15 22:56:31 +0530

    Update maxnoinarr.py

--- maxnoinarr.py
+++ maxnoinarr.py
@@ -1,3 +1,14 @@
+#with using function
+
 a=[12,32,43,34,44,54,76,98,65,78,99]
 b=max(a)
 print("maximam element in the list: ",b)
+
+#without using function
+
+a=[12,32,43,34,44,54,76,98,65,78,99]
+m=a[0]
+for i in a:
+    if(i > m):
+        m= i
+print(m)
