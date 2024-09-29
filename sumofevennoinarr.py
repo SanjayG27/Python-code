6c478a305867a65ebed419aedb47b89d054b7b49
SanjayG27 <sanjaysekarviji27@gmail.com>
2024-09-29 19:31:41 +0530

    Update sumofevennoinarr.py

--- sumofevennoinarr.py
+++ sumofevennoinarr.py
@@ -1,5 +1,3 @@
-#Sum of even numbers in the list
-
 a=[12,32,43,34,44,54,76,98,65,78,99]
 m=0
 for i in a:
@@ -7,13 +5,4 @@ for i in a:
         m+=i
 print("Sum of even no in the arr: ",m)
 
-#With using function
-
-a=[12,32,43,34,44,54,76,98,65,78,99]
-m=[]
-for i in a:
-    if(i%2==0):
-        m.append(i)
-s=sum(m)
-print("Sum of even no in the arr: ",s)
 
